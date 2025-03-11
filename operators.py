85d13ac5990af4b90a1b7a76683b74cc2ebfd7b0
Thone Anlaug <thoneanlaug@gmail.com>
2025-03-11 12:50:53 +0100

    beginning on assignment 4

---
+++ operators.py
@@ -0,0 +1,19 @@
+def OP1():
+    # Remove multiple calls, given a criteria.
+    # The criteria is that a car is full, which is a reason to remove calls.
+    # So if a car is full, then remove a random given number of calls and greedy reinsert them.
+    
+    return
+    
+    
+    
+    
+
+def OP2():
+    # Remove multiple calls, given a criteria.
+    return
+    
+
+def OP3():
+    # Remove multiple calls, given a criteria.
+    return 
