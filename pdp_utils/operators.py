79f1818f5fa4127da02d60632c258263c517a659
Thone Anlaug <thoneanlaug@gmail.com>
2025-03-19 13:43:20 +0100

    jobber med oppg 4

---
+++ pdp_utils/operators.py
@@ -0,0 +1,144 @@
+# Can I only focus on the removal of calls? And creating different ways to remove calls? 
+# And then reinsert the calls in the same way for all the operators.
+# Therefore I can create a function that reinserts the calls, that the operators can use. 
+
+import random
+
+
+# Helping method: This method returns the vehicle ranges.
+def zero_pos(sol):
+    zero_pos = [i for i, x in enumerate(sol) if x == 0]
+    vehicle_ranges = []
+    start_index = 0  
+    
+    # Defines vehicle ranges in the solution
+    for zero in zero_pos:
+        vehicle_ranges.append((start_index, zero)) 
+        start_index = zero + 1
+    vehicle_ranges.append((start_index, len(sol)))
+    
+    return vehicle_ranges
+
+# Helping method: This method adds the removed calls back into the solution, in a Greedy way.
+def greedy_reinsert(calls, sol):
+    # Greedy reinsertion of calls,
+    # Put the call in the best possible position in the solution.
+    # The best possible position is the position that minimizes the cost.
+    best_sol = sol.copy()
+    new_sol = best_sol.copy()
+    
+    for call in calls:
+        best_cost = float('inf')
+        for i in range(len(sol)):
+            new_sol.insert(i, call) # pickup
+            
+            
+            
+            
+            
+        
+    return
+
+# Her kan jeg endre slik at den ser etter vehicle med størst vekt
+def OP1(prob, sol):
+    # Remove multiple calls, given a criteria.
+    # The criteria is that a car is full/not capable to take more calls, which is a reason to remove calls.
+    # So if a car is full, then remove a random given number of calls and greedy reinsert them.
+    
+    # If no cars are full, then remove a random given number of calls from the dummy route.
+    new_sol = sol.copy()
+    vehicles = prob['n_vehicles']
+    vehicle_ranges = zero_pos(sol)
+    
+    possible_calls_to_reinsert = []
+    chosen_vehicle_index = 0
+    biggest_weight = 0
+    
+    for vehicle_index, (start, end) in enumerate(vehicle_ranges):
+        calls = set(new_sol[start:end])
+        calls = list(calls)
+        calls = [x - 1 for x in calls]
+        
+        vehicle_weight = sum(prob['Cargo'][calls, 2]) 
+        
+        if vehicle_weight > biggest_weight:
+                chosen_vehicle_index = vehicle_index 
+                biggest_weight = vehicle_weight
+                calls_to_reinsert = calls
+    
+    # Choosing random number of calls to remove from the possible_calls_to_reinsert
+    num_to_select = random.randint(1, len(possible_calls_to_reinsert))
+    calls_to_reinsert = random.sample(possible_calls_to_reinsert, num_to_select)
+    # Removing the calls from the new solution
+    new_sol = [x for x in new_sol if x not in calls_to_reinsert]
+                
+    return greedy_reinsert(calls_to_reinsert, new_sol) # REMEMBER TO PICKUP AND DELIVER
+    
+    
+    
+    
+
+def OP2():
+    # Remove multiple calls, given a criteria.
+    # Criteria: the car that is the fullest. Altså: has the smallest remaining capacity.
+    
+    return
+    
+
+def OP3():
+    # Remove multiple calls, given a criteria. 
+    # Criterie: Randomly choose a car and remove a random number of calls from that car.
+    return 
+
+
+
+# # Her kan jeg endre slik at den ser etter største vekt under ett gitt call
+# def OP1(prob, sol):
+#     # Remove multiple calls, given a criteria.
+#     # The criteria is that a car is full/not capable to take more calls, which is a reason to remove calls.
+#     # So if a car is full, then remove a random given number of calls and greedy reinsert them.
+    
+#     # If no cars are full, then remove a random given number of calls from the dummy route.
+#     new_sol = sol.copy()
+#     vessel_capacity = prob['VesselCapacity']
+#     vehicles = prob['n_vehicles']
+#     vehicle_ranges = zero_pos(sol)
+    
+#     possible_calls_to_reinsert = []
+#     calls_to_reinsert = []
+#     for vehicle_index, (start, end) in enumerate(vehicle_ranges):
+#         if vehicle_index == vehicles: # This is the dummy route
+#             # posible_calls_to_reinsert += sol[start:end]
+#             # chosen_vehicle_index = vehicle_index
+#             calls_to_reinsert = [] # TODO - Velge et tilfeldig antall calls å fjerne ifra dummy
+#         else:
+#             calls = set(sol[start:end])
+#             calls = list(calls)
+#             calls = [x - 1 for x in calls]
+            
+#             capacity = vessel_capacity[vehicle_index]
+#             vehicle_weight = sum(prob['Cargo'][calls, 2])
+            
+#             calls_for_vehicle = prob['VesselCargo'][vehicle_index]
+#             calls_for_vehicle = [i + 1 for i, x in enumerate(calls_for_vehicle) if x == 1]
+#             print(calls_for_vehicle)
+            
+#             calls_not_in_vehicle = [x for x in calls_for_vehicle if x not in calls]
+#             print(calls_not_in_vehicle)
+            
+#             dict = {}
+#             for call in calls_not_in_vehicle:
+#                 dict[call] = prob['Cargo'][call - 1, 2]
+                        
+#             # Finne minste mulig call for gitt bil, som ikke er i bilen
+#             smallest_possible_call = min(dict, key=dict.get)
+#             # Må kanskje hentes ut på en annen måte,  Må jeg plusse på 1 her for å få riktig bil?
+#             if capacity - vehicle_weight < dict[smallest_possible_call]:
+#                 chosen_vehicle = vehicle_index 
+            
+        
+#         calls_to_reinsert = chosen_vehicle # velg random antall av callsene til chosen vehicle 
+                
+#     return 
+    
+    
--- test.py
+++ test.py
@@ -55,13 +55,97 @@
 
 
 # l = [1,2,3,4,0,5,6,7,8,0,9,10,11,0,12,13,14]
-l = [1,2,0,3]
+# l = [1,2,0,3]
 
-next_zero_index = None
-for i in range(len(l)):
-    for j in range(i, len(l)):
-        if l[j] == 0:
-            next_zero_index = j
-            break
+# next_zero_index = None
+# for i in range(len(l)):
+#     for j in range(i, len(l)):
+#         if l[j] == 0:
+#             next_zero_index = j
+#             break
     
-print(next_zero_index)
+# print(next_zero_index)
+
+from pdp_utils import *
+prob = load_problem('pdp_utils/data/pd_problem/Call_7_Vehicle_3.txt')
+
+
+sol = [1,3,2,3,2,1,0,4,4,0,5,6,5,7,6,7,0]
+
+# calls = sol[0:6]
+calls = set(sol[0:6])
+calls = list(calls)
+calls = [x - 1 for x in calls]
+
+print(calls) # De som er i bilen
+# Calls bilen kan ta 
+vehicle = 1
+calls_for_vehicle = prob['VesselCargo'][vehicle - 1]
+# calls_for_vehicle er binary, så omgjøre til index+1 hvis 1
+calls_for_vehicle = [i + 1 for i, x in enumerate(calls_for_vehicle) if x == 1]
+print(calls_for_vehicle)
+
+# Calls som ikke er i bilen
+calls_not_in_vehicle = [x for x in calls_for_vehicle if x not in calls]
+print(calls_not_in_vehicle)
+# Minste size av call som ikke er i bilen
+dict = {}
+for call in calls_not_in_vehicle:
+    dict[call] = prob['Cargo'][call - 1, 2]
+
+print(dict)
+
+vehicle_weight = sum(prob['Cargo'][calls, 2])
+print(vehicle_weight)
+
+capacity = prob['VesselCapacity'][0]
+
+smallest_possible_call = min(dict, key=dict.get)
+print(smallest_possible_call)
+
+
+# Så lenge det ikke er mer plass i bilen til en ny call så skal bilen bli valgt
+if capacity - vehicle_weight < dict[smallest_possible_call]:
+    chosen_vehicle = vehicle
+    print("capacity: " , capacity)
+    print("vehicle_weight: ", vehicle_weight)
+    print("diff: ", capacity - vehicle_weight)
+    print("Chosen vehicle: ", chosen_vehicle)
+# else:
+#     continue
+
+new_sol = sol.copy()
+
+
+possible_calls_to_reinsert = calls_for_vehicle
+print(possible_calls_to_reinsert)
+calls_to_reinsert = []
+
+new_sol = [x for x in new_sol if x not in calls_to_reinsert]
+
+num_to_select = random.randint(1, len(possible_calls_to_reinsert))
+
+calls_to_reinsert = random.sample(possible_calls_to_reinsert, num_to_select)
+
+new_sol = [x for x in new_sol if x not in calls_to_reinsert]
+new_sol = new_sol + calls_to_reinsert + [8]
+
+print(calls_to_reinsert)
+
+
+def zero_pos(sol):
+    zero_pos = [i for i, x in enumerate(sol) if x == 0]
+    vehicle_ranges = []
+    start_index = 0  
+    
+    # Defines vehicle ranges in the solution
+    for zero in zero_pos:
+        vehicle_ranges.append((start_index, zero)) 
+        start_index = zero + 1
+    vehicle_ranges.append((start_index, len(sol)))
+    
+    return vehicle_ranges
+
+print(new_sol)
+vehicle_ranges = zero_pos(new_sol)
+print(vehicle_ranges)
